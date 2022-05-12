8b6840eda3bdcafa864503d2858003e48b8d3eca
CapSap <cshotam@gmail.com>
2022-05-13 09:23:07 +1000

    added copy to header

--- src/container/Headinng/index.js
+++ src/container/Headinng/index.js
@@ -1,7 +1,7 @@
 const Heading = () => {
   return (
     <div>
-      <h1>Morse Code App</h1>
+      <h1>Morse Code Keyer</h1>
       <p>Turn your dits and dahs into english or vice reversa</p>
     </div>
   );
